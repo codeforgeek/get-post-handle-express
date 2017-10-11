ad3e0c54cc9228f88807db9425e3749e6a4c27f2
Douglas S Elias <douglassantoselias@gmail.com>
2017-10-11 20:55:44 -0300

    Remove node_modules folder and change to ES6 syntax

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var express   =     require("express");
-var bodyParser  =    require("body-parser");
-var app       =     express();
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/',function(req,res){
-  res.sendfile("index.html");
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
 });
 
-app.post('/login',function(req,res){
-  var user_name=req.body.user;
-  var password=req.body.password;
-  console.log("From Client pOST request: User name = "+user_name+" and password is "+password);
-  res.end("yes");
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(`POST request: username is ${username} and password is ${password}`);
+  res.end(`You are now logged in Mr(s) ${username}`);
 });
 
-
-app.listen(3000,function(){
-  console.log("Started on PORT 3000");
-})
+app.listen(3000, () => {
+  console.log("Started on http://localhost:3000");
+});
